810b800c6a8611faa436a5583390d8752e70a331
serge <sergej-93@mail.ru>
2024-07-16 23:59:56 +0600

    Показать дату в длинном формате

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,21 @@
 import { defineComponent, createApp } from 'vue/dist/vue.esm-bundler.js'
+
+const options = {
+  dateStyle: 'long'
+};
+
+const DateLocal = defineComponent({
+  name: 'DateLocal',
+  setup () {
+    function formatAsLocalDate() {
+      return new Date().toLocaleDateString('ru-RU', options)
+    }
+    return {
+      formatAsLocalDate,
+    }
+  },
+  template:  '<div>Сегодня {{ formatAsLocalDate() }}</div>'
+})
+
+createApp(DateLocal)
+  .mount('#app')
